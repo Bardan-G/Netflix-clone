49cd925e58b979c52f7b6f3362cfbf7ffe65dff3
Bardan-g <neupanebardang@gmail.com>
2024-12-28 20:48:00 +0545

    auth half done

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import { useCallback, useState } from "react";
 import Input from "@/Components/Input";
-
+import {signIn} from 'next-auth/react';
+import { error } from 'console';
+import { useRouter } from 'next/router';
 
 const Auth = () => {
+    const router = useRouter();
+
     const [email, setEmail]= useState ('');
     const [name, setName]= useState ('');
     const [password, setPassword]= useState ('');
@@ -11,6 +15,21 @@ const Auth = () => {
     const toggleVariant =useCallback (()=>{
         setVariant((currentVariant)=> currentVariant === 'login' ? 'register' : 'login');
     },[])
+     const login = useCallback(async() =>{
+        try{
+            await signIn('credentials',{
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+        }
+        catch (error) {
+            console.log(error);
+        }
+    },[email, password, router]);
 
     const register = useCallback(async ()=> {
         try {
@@ -19,11 +38,14 @@ const Auth = () => {
                 name,
                 password
 
-            })
+            });
+            login();
         }catch (error) {
             console.log(error);
         }
-    },[email, name, password]);
+    },[email, name, password, login]);
+
+   
   return (
     <div className="relative h-full w-full bg-[url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f562aaf4-5dbb-4603-a32b-6ef6c2230136/dh0w8qv-9d8ee6b2-b41a-4681-ab9b-8a227560dc75.jpg/v1/fill/w_1192,h_670,q_70,strp/the_netflix_login_background__canada__2024___by_logofeveryt_dh0w8qv-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZjU2MmFhZjQtNWRiYi00NjAzLWEzMmItNmVmNmMyMjMwMTM2XC9kaDB3OHF2LTlkOGVlNmIyLWI0MWEtNDY4MS1hYjliLThhMjI3NTYwZGM3NS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.LOYKSxIDqfPwWHR0SSJ-ugGQ6bECF0yO6Cmc0F26CQs')] bg-no-repeat bg-fixed bg-cover ">
         <div className="bg-black h-full w-full lg:bg-opacity-50">
@@ -56,7 +78,7 @@ const Auth = () => {
                     value={password}
                     />
                 </div>
-                <button onClick={register} className="bg-red-600 py-3 text-white rounded-md mt-10 w-full hover:bg-red-700 transation">
+                <button onClick={variant === 'login' ? login :register} className="bg-red-600 py-3 text-white rounded-md mt-10 w-full hover:bg-red-700 transation">
                     {variant === 'login' ? 'login': 'Sign up'}
                 </button>
                 <p className="text-neutral-500 mt-12  ">New to Netflix?
