1dc47195852f2e04a6862156973af0383a96640d
Bardan-g <neupanebardang@gmail.com>
2025-01-06 21:32:22 +0545

    changes

--- Components/Billboard.tsx
+++ Components/Billboard.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Billboard = ()=>{
     const { data } = useBillboard();
     return(
-        <div className="relative h-[56.25vw]">
+        <div className="relative h-[56.25vw] w-[20.vw]">
             <video  src=""></video>
         </div>
     )
