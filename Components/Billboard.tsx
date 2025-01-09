38a1d07f6c9fc7ec8983f6b57f079097224646d2
Bardan-g <neupanebardang@gmail.com>
2025-01-09 21:18:39 +0545

    added

--- Components/Billboard.tsx
+++ Components/Billboard.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Billboard = ()=>{
     const { data } = useBillboard();
     return(
-        <div className="relative h-[56.25vw] w-[20.vw]">
+        <div className="relative h-[56.25vw] w-[20.vw] flex">
             <video  src=""></video>
         </div>
     )
